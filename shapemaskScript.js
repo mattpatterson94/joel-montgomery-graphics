abf4c6651bc1ac800f3692d036668d26ab9f2344
Joel Montgomery <joelmontgomery11@gmail.com>
2024-07-24 13:29:08 +1000

    Update shapemaskScript.js

--- shapemaskScript.js
+++ shapemaskScript.js
@@ -1,20 +1,36 @@
 $(document).ready(function() {
-    // Enable submit button when textarea has input
+    // Initial button state based on textarea content
     toggleButtonState();
-    
+    toggleCopyButtonState();
+
+    // Event listener for textarea input to enable/disable the button
     $('#input').on('input', function() {
         toggleButtonState();
     });
+    $('#output').on('input', function() {
+        toggleCopyButtonState();
+    });
 
+    // Function to toggle button state
     function toggleButtonState() {
         if ($('#input').val().trim() === '') {
-            $('button').prop('disabled', true);
+            $('#submit-button').prop('disabled', true);
         } else {
-            $('button').prop('disabled', false);
+            $('#submit-button').prop('disabled', false);
         }
     }
 
-    // Process SVG code
+    // Function to toggle button state
+    function toggleCopyButtonState() {
+        if ($('#output').val().trim() === '') {
+            $('#copy-button').prop('disabled', true);
+        } else {
+            $('#copy-button').prop('disabled', false);
+        }
+    }
+
+
+    // Existing click event listener for the button
     $('button').click(function() {
         var input = $('#input').val();
         var output = $.parseHTML(input);
@@ -36,5 +52,24 @@ $(document).ready(function() {
             .prop('outerHTML')
             .replaceAll('image1', 'image');
         $('#output').val(outputHTML);
+
+        toggleCopyButtonState();
+
     });
 });
+
+function copyOutput() {
+    // Get the text field
+    var copyText = document.getElementById("output");
+  
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+  
+     // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+
+    // toggle button clicked
+    var button = document.getElementById('copy-button-content');
+    button.classList.toggle('clicked');
+  }
